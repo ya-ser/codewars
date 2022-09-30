8ce0d57049f5262a353cd9bf50b8c59b8fd4c49e
Yaser Ibrahim <yaser.a.ibrahim@gmail.com>
2022-09-30 12:17:48 -0400

    solution for check same case challenge

---
+++ 8kyu/checkSameCase.js
@@ -0,0 +1,30 @@
+/* Check Same Case
+
+Write a function that will check if two given characters are the same case.
+
+If either of the characters is not a letter, return -1
+If both characters are the same case, return 1
+If both characters are letters, but not the same case, return 0
+Examples
+'a' and 'g' returns 1
+
+'A' and 'C' returns 1
+
+'b' and 'G' returns 0
+
+'B' and 'g' returns 0
+
+'0' and '?' returns -1
+
+*/
+
+function sameCase(a, b) {
+  // check if both params are letters and uppercase
+  if (!/[a-zA-Z]/.test(a) || !/[a-zA-Z]/.test(b)) {
+    return -1;
+  } else if (/[a-z]/.test(a) && /[a-z]/.test(b) || /[A-Z]/.test(a) && /[A-Z]/.test(b)) {
+    return 1;
+  } else {
+    return 0;
+  }
+};
