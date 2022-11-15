71dd50a50dd2dcac3b9c46fdff5ebcfad8513ca5
Yaser Ibrahim <yaser@Yasers-MacBook-Pro.local>
2022-11-15 11:20:34 -0500

    solution for highest and lowest challenge

---
+++ 7kyu/highestAndLowest.js
@@ -0,0 +1,16 @@
+/* Highest and Lowest
+
+In this little assignment you are given a string of space separated numbers, and have to return the highest and lowest number.
+
+Examples:
+
+highAndLow("1 2 3 4 5");  // return "5 1"
+highAndLow("1 2 -3 4 5"); // return "5 -3"
+highAndLow("1 9 3 4 -5"); // return "9 -5"
+
+*/
+
+function highAndLow(numbers){
+  numbers = numbers.split(' ');
+  return `${Math.max(...numbers)} ${Math.min(...numbers)}`;
+}
